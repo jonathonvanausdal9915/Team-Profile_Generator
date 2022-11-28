6969183594bf6b2ac76580064aeeebe21c460a99
Jonathon <>
2022-11-27 23:01:59 -0700

    generateHTML function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const generateHTML = require('./src/generateHTML');
+
 const fs = require("fs");
 const inquirer = require('inquirer');
 
